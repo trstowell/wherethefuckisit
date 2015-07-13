ab4a881d683616fc09dfa40d4d5b3143ff4478a5
trstowell <trstowell@gmail.com>
2015-07-13 06:38:57 -0400

    adding contact icons to home.html

--- static/js/base.js
+++ static/js/base.js
@@ -1,1 +1,25 @@
-var app = angular.module("trstowell", ['ui.bootstrap']);
+var app = angular.module("SWIM", ['ui.bootstrap','ngRoute']);
+
+app.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
+
+app.config(['$interpolateProvider', function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[');
+  $interpolateProvider.endSymbol(']}');
+}]);
+
+app.config( function($routeProvider, $locationProvider) {
+     $routeProvider.
+         when('/', {
+             templateUrl: '/static/partials/home.html'
+         }).
+         otherwise({
+             redirectTo: '/'
+         });
+
+         $locationProvider.html5Mode(true);
+
+});
+
