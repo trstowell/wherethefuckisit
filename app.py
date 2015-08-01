f41863ecf5a38fb90f5b3e98ce4dd6c9a73e0759
trstowell <trstowell@gmail.com>
2015-08-01 02:39:22 -0400

    adding /text endpoint

--- app.py
+++ app.py
@@ -8,5 +8,19 @@ app = Flask(__name__, static_url_path="/static")
 def index(path):
     return render_template('index.html')
 
+@app.route('/text/<numbers_raw>/<body>')
+def text(numbers_raw, body):
+    #TODO better sanitize numbers for twilio format
+    account = "AC3db39e35c90d6a6cd40e6a42614e9239"
+    token = "5c0684968b8d21709838a16afbf40493"
+    client = TwilioRestClient(account, token)
+
+    numbers = [str(num).replace("-","").replace("(","").replace(")","") for num in numbers_raw.split('+')]
+
+    for number in numbers:
+        message = client.messages.create(to=number, from_="+15084396156", body=str(body))
+
+    return "Sent"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
